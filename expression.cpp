b2c9fbe3b6193fb62233a7cf61393830d38713bd
LeeX <a543748616@gmail.com>
2017-03-22 12:07:06 +0800

    Chapter 4 update

--- expression.cpp
+++ expression.cpp
@@ -113,9 +113,98 @@ int main(int argc, const char * argv[]) {
     } else
         cout << "pass" << endl;*/
     
+//    建议仅将位运算符作用于无符号类型
+//    通过位运算符来修改成绩通过与否
+    /*unsigned long quiz1 = 0;
+//    unsigned long类型的值可以保证一定占32位
+    quiz1 |= 1UL << 27;
+//    假设只有27号一个人没有通过，其他人全部通过
+    quiz1 = ~(1UL << 27);
+//    检测学生的通过状态
+    bool status = quiz1 & (1UL << 27);
+    if(status) {
+        cout << "pass" << endl;
+    } else {
+        cout << "fail" << endl;
+    }*/
     
+//    sizeof运算符不会将char数组当作指针处理，对于char会返回1
+    /*string s1 = "I'm a pig!!";
+    char c[] = {'p', 'i', 'g'};
+    auto p = sizeof(s1);
+    auto x = sizeof(c);
+    auto m = x/sizeof(*c);
+    cout << p << endl;
+    cout << x << m << endl;*/
     
+//    输出每一种内置类型所占的大小
+    /*bool b = 1;
+    char c = 'a';
+    wchar_t wc = 'c';
+    char16_t c16 = 'p';
+    char32_t c32 = 'p';
+    short s = 14;
+    int i = 4582;
+    long l = 74838485;
+    long long ll = 75638;
+    float f = 9.43;
+    double d = 23.8475;
+    long double ld = 33274.74759303;
     
+    size_t sb = sizeof(b);          //1
+    size_t sc = sizeof(c);          //1
+    size_t swc = sizeof(wc);        //4
+    size_t sc16 = sizeof(c16);      //2
+    size_t sc32 = sizeof(c32);      //4
+    size_t ss = sizeof(s);          //2
+    size_t si = sizeof(i);          //4
+    size_t sl = sizeof(l);          //8
+    size_t sll = sizeof(ll);        //8
+    size_t sf = sizeof(f);          //4
+    size_t sd = sizeof(d);          //8
+    size_t sld = sizeof(ld);        //16
+    
+    cout << sb << " " << sc << " " << swc << " " << sc16 <<
+    " " << sc32 << " " << ss << " " << si << " " << sl << " " <<
+    sll << " " << sf << " " << sd << " " << sld;*/
+    
+//    一个整型指针占8位，一个整型数值占4位，所以sizeof的运算对象是无效指针也是安全行为，
+//    因为它返回的是某个类型的指针所占的空间，而指针所指向的对象的类型的值也是确定的
+    /*int x[10];
+    int *p = x;
+    int *m;
+    cout << sizeof(x)/sizeof(*x) << endl;
+    cout << sizeof(*p) << endl;
+    cout << sizeof(m) << endl;*/
+    
+//    前置运算符保证下一次赋值永远是上一次的前一位的索引位置
+    /*vector<unsigned long> vec{4, 2, 6, 34, 63};
+    vector<unsigned long>::size_type cnt = vec.size();
+    for(vector<unsigned long>::size_type i = 0; i != vec.size(); ++i, --cnt) {
+        vec[i] = cnt;
+    }
+    for(auto c : vec) {
+        cout << c << " " << endl;
+    }*/
+    
+//    如果后置运算符放在for循环里，那么每次赋值操作会进行两遍，所以应该在循环体内完成运算
+    /*vector<unsigned long> vec{4, 2, 6, 34, 63};
+    vector<unsigned long>::size_type cnt = vec.size();
+    for(vector<unsigned long>::size_type i = 0; i != vec.size();) {
+        vec[i] = cnt;
+        i++;
+        cnt--;
+    }
+    for(auto c : vec) {
+        cout << c << " " << endl;
+    }*/
+    
+//    循环的判断条件都是遍历完整个数组
+    /*constexpr int size = 5;
+    int ia[size] = {1, 2, 3, 4, 5};
+    for(int *p = ia, ix = 0; ix != size && p != ia + size; ++ix, ++p) {
+//        ...
+    }*/
     
     return 0;
 }
