00138299f4b6e739d599f28bf7028fadb287889a
LeeX <a543748616@gmail.com>
2017-03-25 23:35:13 +0800

    Chapter6 function in this .h file

---
+++ function.h
@@ -0,0 +1,9 @@
+#ifndef FUNCTION_H
+#define FUNCTION_H
+int fact(int val);
+int mutualFact();
+int recursionFact(int val);
+double absoluteValue(double val);
+size_t unmeaning(size_t val);
+unsigned staticTest();
+#endif
