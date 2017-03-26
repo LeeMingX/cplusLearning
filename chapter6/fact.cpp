565f7a7ac4f0cdf9967cde1471b9e9eb1033bff2
LeeX <a543748616@gmail.com>
2017-03-27 01:03:42 +0800

    Chapter 6 function realization update

--- chapter6/fact.cpp
+++ chapter6/fact.cpp
@@ -12,7 +12,6 @@
 #include <string>
 #include <cctype>
 #include <cstring>
-
 using std::cin; using std::cout; using std::cerr;
 using std::endl;
 using std::string;
@@ -48,7 +47,7 @@ int recursionFact(int val) {
     if(val == 0 || val == 1) {
         return 1;
     } else {
-        return fact(--val) * (val + 1);
+        return fact(val - 1) * val;
     }
 }
 
@@ -141,3 +140,72 @@ void swapPointerWithPointer(int **p1, int **p2) {
     *p1 = *p2;
     *p2 = temp;
 }
+
+void printWithInteger(const int *ia, int val) {
+    if(ia) {
+        while (*ia != val) {
+            cout << *ia << endl;
+            ++ia;
+        }
+    }
+}
+
+void pringWithPointer(const int *beg, const int *end, int val) {
+    for (; beg != end; ++beg) {
+        if(*beg != val) {
+            cout << *beg << endl;
+        }
+    }
+}
+void printWithFor(const int ia[], size_t n, int val) {
+    for(size_t i = 0; i != n; ++i) {
+        if(ia[i] != val) {
+            cout << ia[i] << endl;
+        }
+    }
+}
+
+unsigned calArgvSum(initializer_list<int> il) {
+    auto beg = il.begin();
+    auto end = il.end();
+    unsigned sum = 0;
+    while (beg != end) {
+        sum += *beg;
+        ++beg;
+    }
+    
+    return sum;
+}
+
+//测试当return没有返回值和函数缺少return值的函数
+/*bool str_subrange(const string &str1, const string &str2) {
+    if(str1.size() == str2.size())
+        return str1 == str2;
+    auto size = str1.size() < str2.size() ? str1.size() : str2.size();
+    for (decltype(str1.size()) i = 0; i < size; ++i) {
+        if(str1[i] != str2[i]) {
+            return 0;
+        }
+    }
+}*/
+
+int &get(int *p, int index) {
+    return p[index];
+}
+
+void printVectorWithRecursion(const vector<string> &vec, vector<string>::size_type index) {
+    if(index > 0) {
+        cout << vec[index] << endl;
+        printVectorWithRecursion(vec, index - 1);
+    } else
+        cout << vec[index] << endl;
+}
+
+//在函数声明文件中只需要声明返回类型即可，定义文件中可以用不同的方式声明返回类型
+//c++不允许重复定义,头文件中声明好的函数没有定义，编译器会报错
+int odd[] = {1, 3, 5, 7, 9};
+int even[] = {0, 2, 4, 6, 8};
+decltype(odd) &arrPtr(int i) {
+    return i % 2 == 0 ? odd : even;
+}
+
