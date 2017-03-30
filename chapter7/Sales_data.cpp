da34f572f51d675b4f7d8928408579ce4f280512
LeeX <a543748616@gmail.com>
2017-03-30 12:28:36 +0800

    New Sales_data extern and definition

---
+++ chapter7/Sales_data.cpp
@@ -0,0 +1,41 @@
+//
+//  Sales_data.cpp
+//  cplusplusLearn
+//
+//  Created by 李璇 on 2017/3/30.
+//  Copyright © 2017年 李璇. All rights reserved.
+//
+
+#include "Sales_data.h"
+
+double Sales_data::avg_Price() const {
+    if(saleNum) {
+        return revenue / saleNum;
+    } else
+        return 0;
+}
+
+Sales_data& Sales_data::combine(const Sales_data &rhs) {
+    saleNum += rhs.saleNum;
+    revenue += rhs.revenue;
+    return *this;
+}
+
+Sales_data add(const Sales_data& lhs, const Sales_data& rhs) {
+    Sales_data sum = lhs;
+    sum.combine(rhs);
+    return sum;
+}
+
+std::istream& read(std::istream& is, Sales_data& sd) {
+    is >> sd.bookNo >> sd.saleNum >> sd.price;
+    sd.revenue = sd.saleNum * sd.price;
+    return is;
+    
+}
+
+std::ostream& print(std::ostream& os, const Sales_data& sd) {
+    os << sd.isbn() << " " << sd.saleNum << " " << sd.revenue << " "
+    << sd.avg_Price();
+    return os;
+}
---
+++ chapter7/Sales_data.h
@@ -0,0 +1,29 @@
+//
+// Created by lxGhost on 2017/3/14.
+//
+
+//头文件保护符
+#ifndef Sales_data_h
+#define Sales_data_h
+
+#include <string>
+#include <iostream>
+using std::string;
+
+struct Sales_data {
+    string bookNo;                              //书的ISBN号
+    string bookName;                            //书的名字
+    unsigned saleNum = 0;                       //书的销售数量
+    double price = 0.0;                         //书的销售单价
+    double revenue =0.0;                        //书的销售额
+    
+    string isbn() const { return bookNo; };     //返回ISBN号
+    Sales_data& combine(const Sales_data&);     //将一个对象加到另外一个上面
+    double avg_Price() const;                   //求售出的书籍的平均价格
+};
+
+Sales_data add(const Sales_data&, const Sales_data&);       //执行两个对象的加法
+std::istream& read(std::istream&, Sales_data&);             //将数据从istream读入到对象中
+std::ostream& print(std::ostream&, const Sales_data&);       //将对象的值输出到ostream
+
+#endif
